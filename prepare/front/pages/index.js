a9d3ada665e18a203d7f1d1c17d1cb5ad25146cc
seojinwoong <tjwlsdnd12@naver.com>
2022-01-05 18:48:47 +0900

    20210104 커밋

--- prepare/front/pages/index.js
+++ prepare/front/pages/index.js
@@ -1,9 +1,19 @@
 import AppLayout from "../components/AppLayout";
+import { useSelector } from "react-redux";
+import PostForm from "../components/PostForm";
+import PostCard from '../components/PostCard';
 
 const Home = () => {
+    const { isLoggedIn } = useSelector(state => state.user);
+    // 또는 ,,, 이렇게도 가능 => const isLoggedIn = useSelector(state => state.user.isLoggedIn);
+
+    const { mainPosts } = useSelector(state => state.post);
+    // 또는 ,,, 이렇게도 가능 => const mainPosts = useSelector(state => state.post.mainPosts);
+
     return (
         <AppLayout>
-            <div>Hello, Next!</div>
+            {isLoggedIn && <PostForm />}
+            {mainPosts.map((post, index) => <PostCard key={post.id} post={post}/>)}
         </AppLayout>
     )
 }
--- prepare/front/reducers/post.js
+++ prepare/front/reducers/post.js
@@ -1,9 +1,58 @@
 export const initialState = {
-    mainPosts: []
+    mainPosts: [{
+        id: 1,
+        User: {
+            id: 1,
+            nickname: '제로초'
+        },
+        content: '첫번째 게시글 #해시태그 #익스프레스',
+        Images: [{
+            src: 'https://picsum.photos/200/300'
+        }, {
+            src: 'https://picsum.photos/id/237/200/300'
+        }, {
+            src: 'https://picsum.photos/seed/picsum/200/300'
+        }],
+        Comments: [{
+            User: {
+                nickname: 'nero'
+            },
+            content: '우와 개정판이 나왔군요!'
+        }, {
+            User: {
+                nickname: 'hero'
+            },
+            content: '얼른 사고싶어요~'
+        }]
+    }],
+    imagePaths: [],
+    postAdded: false
+};
+
+const ADD_POST = 'ADD_POST';
+export const addPost = {
+    type: ADD_POST,
+};
+
+const dummyPost = {
+    id: 2,
+    content: '더미데이터입니다',
+    User: {
+        id: 1,
+        nickname: '제로초'
+    },
+    Images: [],
+    Comments: []
 };
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+        case ADD_POST:
+            return {
+                ...state,
+                mainPosts: [dummyPost, ...state.mainPosts],
+                postAdded: true
+            }
         default:
             return state;
     }
